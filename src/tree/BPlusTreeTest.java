735517446a15422d773bd6e971bef0e5a236341a
Helena Kamali <92225996+helenakm@users.noreply.github.com>
2025-03-22 11:43:49 -0400

    Update BPlusTreeTest.java
    Changed the file path

--- src/tree/BPlusTreeTest.java
+++ src/tree/BPlusTreeTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class BPlusTreeTest {
     public static void main(String[] args) {
-    	 String filePath = "/Users/helenakamali/Desktop/Winter2025/EECS4411/BPlusTreeProject/src/3000_random_numbers-2.txt"; 
+    	 String filePath = "/.."; 
     	 
 	        List<Integer> numberList = new ArrayList<>();
 	        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
@@ -50,37 +50,5 @@ public class BPlusTreeTest {
             e.printStackTrace();
         }
     }
-
-//        tree.insert(2);
-//        tree.insert(3);
-//        tree.insert(5);
-//        tree.insert(7);
-//        tree.insert(14);
-//        tree.insert(16);
-//        tree.insert(19);
-//        tree.insert(20);
-//        tree.insert(22);
-//        tree.insert(4);
-//        tree.insert(8);
-//        tree.insert(24);
-//        tree.insert(27);
-//        tree.insert(29);
-//        tree.insert(33);
-//        tree.insert(34);
-//        tree.insert(38);
-//        tree.insert(39);
-//        tree.insert(9);
-//        tree.insert(10);
-//    
-//        tree.setPrintTreeEnabled(true);
-//        System.out.println("Tree after insertion:");
-//        tree.printTree();
-    
-//        //test for search
-//        System.out.println("Search for 34: " + tree.search(34));  // True
-//        System.out.println("Search for 25: " + tree.search(25));  // False
-//        System.out.println("Search for 55: " + tree.search(55));
-//        System.out.println("Search for 101: " + tree.search(101));
-//    }
     
 }
